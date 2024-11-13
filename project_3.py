1dfb828c69a178145873cfff3f4eba8d0b737259
pyshkovni <guess.the.tutor@gmail.com>
2024-11-13 08:48:22 +0300

    Описал задания проектов

---
+++ project_3.py
@@ -0,0 +1,13 @@
+# project_3
+
+# Есть папки с фотографиями товаров.
+# Например, в папке А находятся папки 1, 2, 3. В каждой из них находится по 3 картинки.
+# Отсортируйте картинки так, чтобы в папке А первые по номеру картинки из папок 1, 2, 3 
+# оказались в новой папке 1 с новыми именами и так далее.
+# Используйте библиотеку os
+
+# Например,
+# https://drive.google.com/file/d/1t8Ba9jRBsSOLPntvTajVv0jaXrhGHr8w/view?usp=sharing
+
+# Ссылка на файлы:
+# https://drive.google.com/drive/folders/1nbqg0cjNbjaDX8KfwD5xIMt3YuWMByI8?usp=sharing
