2c199ed0a281b9832cc84a6f8231806421475348
pyshkovni <guess.the.tutor@gmail.com>
2024-12-03 19:38:10 +0300

    поправил ошибки в пакетах проектов

--- project_4.py
+++ project_4.py
@@ -6,7 +6,7 @@
 # Например, 
 # 8(645) 590-10-12 -> 86455901012.  
 
-# Используйте библиотеку pandas.
+# Используйте библиотеку pandas, openpyxl
 
 # Ссылка на файл.
 # https://drive.google.com/drive/folders/1FpCRKyCTgAuWS9h2O0tWmJZ5erTPYxQ1?usp=sharing
